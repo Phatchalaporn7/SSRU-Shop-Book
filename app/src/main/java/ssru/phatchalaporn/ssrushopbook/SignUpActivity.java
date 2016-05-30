785738d8b774bc01bf105bbbb3f62aec782958f8
milk <milk111milk@hotmail.com>
2016-05-30 15:07:19 +0700

    Check Space

--- app/src/main/java/ssru/phatchalaporn/ssrushopbook/SignUpActivity.java
+++ app/src/main/java/ssru/phatchalaporn/ssrushopbook/SignUpActivity.java
@@ -1,13 +1,55 @@
 package ssru.phatchalaporn.ssrushopbook;
 
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวเเปร
+    private EditText nameEditText, surEditText, userEditText, passwordEditText;
+    private String nameString, surnameString, userString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind Widget การผูกตัวแปรที่ประกาศไว้
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+
+
+    } //Main Method
+
+    public void clickSingUpSign(View view){
+        nameString = nameEditText.getText().toString().trim();
+        surnameString = surEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space ตรวจสอบว่ามีการกรอกครบไหม
+        if (nameString.equals("") || surnameString.equals("") ||
+                userString.equals("") ||passwordString.equals("")) {
+            //Have Space เงื่อนไขที่มีช่องว่าง
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณาตอบทุกช่อง ค่ะ!!");
+
+
+        } else {
+
+            //No Space เงื่อนไขไม่มีช่องว่าง
+        }
+
+
+
+
+    }// clickSign
+
+}//Main class
