3c12679a8d16e8a650172a3fbd963f3c6a605c54
milk <milk111milk@hotmail.com>
2016-05-31 10:14:54 +0700

    Upload finish

--- app/src/main/java/ssru/phatchalaporn/ssrushopbook/SignUpActivity.java
+++ app/src/main/java/ssru/phatchalaporn/ssrushopbook/SignUpActivity.java
@@ -7,6 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUpActivity extends AppCompatActivity {
 
     //Explicit ประกาศตัวเเปร
@@ -53,6 +63,31 @@ public class SignUpActivity extends AppCompatActivity {
     }// clickSign
 
     private void uploadNewUser() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("Surname", surnameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url("http://swiftcodingthai.com/ssru/add_user_milk.php").post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
+
+
 
 
 
