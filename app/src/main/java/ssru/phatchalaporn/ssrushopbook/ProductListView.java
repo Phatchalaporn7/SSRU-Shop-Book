645589123fa54b074972ea02239e87ec58d9007f
milk <milk111milk@hotmail.com>
2016-06-01 13:52:53 +0700

    ListView success

--- app/src/main/java/ssru/phatchalaporn/ssrushopbook/ProductListView.java
+++ app/src/main/java/ssru/phatchalaporn/ssrushopbook/ProductListView.java
@@ -1,9 +1,20 @@
 package ssru.phatchalaporn.ssrushopbook;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -12,7 +23,8 @@ public class ProductListView extends AppCompatActivity {
     //ประการศตัวแปร
     private TextView nametextView, surnameTextView, moneyTextView;
     private ListView listView;
-    private String[] loginStrings;
+    private String[] loginStrings,nameStrings,priceStrings, coverStrings, eBookStrings;
+    private String urlJSON = "http://swiftcodingthai.com/ssru/get_product.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +44,90 @@ public class ProductListView extends AppCompatActivity {
         surnameTextView.setText(loginStrings[2]);
         moneyTextView.setText(loginStrings[5] + "THB.");
 
-
+        SynchronizProduct synchronizgProduct = new SynchronizProduct(this, urlJSON);
+        synchronizgProduct.execute();
     }// Main Method
+
+    private class SynchronizProduct extends AsyncTask<Void, Void, String> {
+
+        private Context context;
+        private String urlString;
+        private ProgressDialog progressDialog;
+
+
+        public SynchronizProduct(Context context,
+                                  String urlString) {
+            this.context = context;
+            this.urlString = urlString;
+        }//Constructor
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(context, "Load Product",
+                    "Load Product Process ...");
+
+        } //onPre
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlString).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                Log.d("1JuneV1", "doIn e ==>" + e.toString());
+                return null;
+            }
+
+
+        } //doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if (s.length() != 0) {
+                progressDialog.dismiss();
+                Log.d("1JuneV1", "s ==>" + s);
+            }
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                nameStrings = new String[jsonArray.length()];
+                priceStrings = new String[jsonArray.length()];
+                coverStrings = new String[jsonArray.length()];
+                eBookStrings = new String[jsonArray.length()];
+
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    nameStrings[i] = jsonObject.getString("Name");
+                    priceStrings[i] = jsonObject.getString("Price");
+                    coverStrings[i] = jsonObject.getString("Cover");
+                    eBookStrings[i] = jsonObject.getString("Ebook");
+                }//for
+
+                MyAdapter myAdapter = new MyAdapter(context, nameStrings,
+                        priceStrings, coverStrings);
+                listView.setAdapter(myAdapter);
+
+            } catch (Exception e) {
+                Log.d("1JuneV2", "onPost e ==> " + e.toString());
+            }
+
+        }// onPost
+    }
+
+
 
 }//Main Class
