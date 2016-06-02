45a106aa3b997e147ac3d4af1f8c7cb3bf871a45
milk <milk111milk@hotmail.com>
2016-06-02 10:55:51 +0700

    UpDate

--- app/src/main/java/ssru/phatchalaporn/ssrushopbook/ProductListView.java
+++ app/src/main/java/ssru/phatchalaporn/ssrushopbook/ProductListView.java
@@ -14,13 +14,19 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.EmptyStackException;
 import java.util.List;
 
 public class ProductListView extends AppCompatActivity {
@@ -30,6 +36,7 @@ public class ProductListView extends AppCompatActivity {
     private ListView listView;
     private String[] loginStrings,nameStrings,priceStrings, coverStrings, eBookStrings;
     private String urlJSON = "http://swiftcodingthai.com/ssru/get_product.php";
+    private String moneyString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +50,7 @@ public class ProductListView extends AppCompatActivity {
 
         //Receive Value From Intent
         loginStrings = getIntent().getStringArrayExtra("Login");
-
+        moneyString = loginStrings[5];
         //show View
         nametextView.setText(loginStrings[1]);
         surnameTextView.setText(loginStrings[2]);
@@ -52,6 +59,76 @@ public class ProductListView extends AppCompatActivity {
         SynchronizProduct synchronizgProduct = new SynchronizProduct(this, urlJSON);
         synchronizgProduct.execute();
     }// Main Method
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.d("2JuneV1", "onRestart Work");
+
+        SynUserTABLE synUserTABLE = new SynUserTABLE();
+        synUserTABLE.execute();
+    }
+
+    private class SynUserTABLE extends AsyncTask<Void, Void, String> {
+
+        private String myResult = null;
+
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                RequestBody requestBody = new FormEncodingBuilder()
+                        .add("isAdd", "true")
+                        .add("User", loginStrings[3])
+                        .build();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url("http://swiftcodingthai.com/ssru/get_user_where.php")
+                        .post(requestBody).build();
+                Call call = okHttpClient.newCall(request);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Request request, IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(Response response) throws IOException {
+                        Log.d("2JuneV1", "response ==> " + response.body().string());
+
+
+
+                        try {
+                            JSONArray jsonArray = new JSONArray(response.body().string());
+                            JSONObject jsonObject = jsonArray.getJSONObject(0);
+                            moneyString = jsonObject.getString("Money");
+
+                            Log.d("2JuneV1", "moneyString ==> " + moneyString);
+
+                            moneyTextView.setText(moneyString + "THB.");
+
+                        } catch (Exception e) {
+                            Log.d("2JuneV1", "e ==> " + e.toString());
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+
+            } catch (Exception e) {
+                Log.d("2JuneV1", "doIn Error ==> " + e.toString());
+                return null;
+            }
+
+            return null;
+
+        }// doInBack
+
+
+    }  //class Syn
 
     private class SynchronizProduct extends AsyncTask<Void, Void, String> {
 
@@ -150,7 +227,7 @@ public class ProductListView extends AppCompatActivity {
 
         private boolean checkMoney(String priceString) {
 
-            int intMyMoney = Integer.parseInt(loginStrings[5]);
+            int intMyMoney = Integer.parseInt(moneyString);
             int intPrice = Integer.parseInt(priceString);
 
 
@@ -190,6 +267,7 @@ public class ProductListView extends AppCompatActivity {
                 intent.putExtra("NameBook", nameString);
                 intent.putExtra("PriceBook", priceString);
                 intent.putExtra("urlEBook", eBookString);
+                startActivity(intent);
                 dialogInterface.dismiss();
             }
         });
