016399dfe134dab8e3010d968827d6701ed4a243
milk <milk111milk@hotmail.com>
2016-06-02 14:14:44 +0700

    Map มาคสถานที่

--- app/src/main/java/ssru/phatchalaporn/ssrushopbook/MapsActivity.java
+++ app/src/main/java/ssru/phatchalaporn/ssrushopbook/MapsActivity.java
@@ -26,7 +26,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private String[] loginStrings;
     private String urlEdit = "http://swiftcodingthai.com/ssru/edit_money_master.php";
     private GoogleMap mMap;
-
+    private double centerLat = 13.775787;
+    private double centerLng = 100.508542;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,12 +78,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng latLng = new LatLng(centerLat, centerLng);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                mMap.clear();
+
+                mMap.addMarker(new MarkerOptions()
+                .position(latLng));
+
+
+            }// on maps Click
+        });
+
     }// onMap
 }// Main class
